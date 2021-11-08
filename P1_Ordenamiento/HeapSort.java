a7bb171a7d7cf5292c5c5a072e043f38f7c2f131
Marco David Chávez Chambi <83045424+MarcXZ18@users.noreply.github.com>
2021-11-07 22:45:43 -0500

    Complejidad de codigo

--- P1_Ordenamiento/HeapSort.java
+++ P1_Ordenamiento/HeapSort.java
@@ -9,11 +9,11 @@ public class HeapSort <T extends Comparable<T>>{
   			System.out.print(array[i] + " ");
 	}
 	
-	public static <T extends Comparable<T>> void heapSort(T array[]) {
-		buildHeap(array);
+	public static <T extends Comparable<T>> void heapSort(T array[]) {  // O(nlogn)
+		buildHeap(array);              
 		int n = array.length;
-		for (int i = n-1; i > 0; i--) {
-			T swap = array[0];
+		for (int i = n-1; i > 0; i--) {       // elementos de extraccion
+			T swap = array[0];            // cambio
 			array[0] = array[i];
 			array[i] = swap;
 			
@@ -21,17 +21,19 @@ public class HeapSort <T extends Comparable<T>>{
 		}
 	}
  
-	public static <T extends Comparable<T>> void heapify(T array[], int n /*tamaño del arbol*/, int i) {
+	public static <T extends Comparable<T>> void heapify(T array[], int n, int i) {  // O(logn)
 		int largest = i; // inicializamos el mas grande como raiz
 		int left = 2*i + 1;
 		int right = 2*i + 2; 
-		
+		// hijo izquierdo existe y si es mayor que la raiz
 		if (left < n && array[left].compareTo(array[largest]) > 0) {
 			largest = left;
 		}
+		// hijo derecho existe y si es mayor que la raiz
 		if (right < n && array[right].compareTo(array[largest]) > 0) {
 			largest = right;
 		}
+		// cambiar la raiz si es necesario
 		if (largest != i) {
 			T swap = array[i];
 			array[i] = array[largest];
@@ -41,9 +43,9 @@ public class HeapSort <T extends Comparable<T>>{
 		}
 	}
 	
-	public static <T extends Comparable<T>> void buildHeap (T array[]) {
+	public static <T extends Comparable<T>> void buildHeap (T array[]) {  // O(n)
 		int n = array.length;
-		for(int i = n/2 - 1; i >= 0; i--) {
+		for(int i = n/2 - 1; i >= 0; i--) {  // construccion del heap maximo
 			heapify(array, n, i);
 		}
 	}
